a83236d261ecee3af5210d10a4d6ff798c74985f
Kibiru-art <kibirujackson@gmail.com>
2026-04-13 15:17:19 +0300

    Recent

--- backend/tickit_backend/urls.py
+++ backend/tickit_backend/urls.py
@@ -1,17 +1,14 @@
 from django.contrib import admin
 from django.urls import path, include
-from django.http import JsonResponse
+from django.http import HttpResponseRedirect
 from django.conf import settings
 from django.conf.urls.static import static
 
 def home(request):
-    return JsonResponse({
-        "message": "TickIT backend is running 🚀",
-        "status": "success"
-    })
+    return HttpResponseRedirect("/api/")
 
 urlpatterns = [
-    path('', home),  # 👈 ADD THIS LINE
+    path('', home),   # 👈 now / redirects to /api/
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
 ]
