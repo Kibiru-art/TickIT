c557648215f1c3ca855452d762b2d2d6fb7cb723
Kibiru-art <kibirujackson@gmail.com>
2026-04-13 15:11:19 +0300

    backend home urls

--- backend/tickit_backend/urls.py
+++ backend/tickit_backend/urls.py
@@ -1,12 +1,19 @@
-# tickit_backend/urls.py
 from django.contrib import admin
 from django.urls import path, include
+from django.http import JsonResponse
 from django.conf import settings
 from django.conf.urls.static import static
 
+def home(request):
+    return JsonResponse({
+        "message": "TickIT backend is running 🚀",
+        "status": "success"
+    })
+
 urlpatterns = [
+    path('', home),  # 👈 ADD THIS LINE
     path('admin/', admin.site.urls),
-    path('api/', include('api.urls')),          # all api endpoints come from api/urls.py
+    path('api/', include('api.urls')),
 ]
 
 if settings.DEBUG:
